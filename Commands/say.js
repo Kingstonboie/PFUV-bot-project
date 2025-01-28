881c350e11362e1bd657f27b9213082eaea8ba90
heavensfear <85403286+Kingstonboie@users.noreply.github.com>
2025-01-28 19:34:56 +0000

    command fix 2

--- Commands/say.js
+++ Commands/say.js
@@ -1,4 +1,4 @@
-exports.run = (client, message, args) => {\
+exports.run = (client, message, args) => {
 let toSay = args.join(" ")
 if(!toSay) return message.channel.send({content: "You have to provide something!"})
 message.channel.send({content: toSay})
