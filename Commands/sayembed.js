811aa1c15da2d2cde76b3b82d91592c09f89d4d7
heavensfear <85403286+Kingstonboie@users.noreply.github.com>
2025-01-29 10:18:04 +0000

    sayembed  fix

--- Commands/sayembed.js
+++ Commands/sayembed.js
@@ -1,15 +1,13 @@
+const { EmbedBuilder } = require("discord.js");
 module.exports = {
   name: "sayembed", 
   run: (client, message, args) => {
     const toSay = args.join(" "); 
     if (!toSay) return message.channel.send({ content: "You have to provide something!" }); 
-
-   
-    const embed = new Discord.EmbedBuilder()
+    const embed = new EmbedBuilder()
       .setDescription(toSay) 
       .setColor("#51C6A5") 
-      .setFooter({ text: `Requested by ${message.author.tag}`, iconURL: message.author.displayAvatarURL() }); 
-
+      .setFooter({ text: `Requested by ${message.author.tag}`, iconURL: message.author.displayAvatarURL() });
     message.channel.send({ embeds: [embed] });
   },
 };
--- index.js
+++ index.js
@@ -1,49 +1,3 @@
-/*
-const express = require("express"); 
-const app = express();
-const keep_alive = require('./keep_alive.js')
-
-app.listen(3000, () => {
-  console.log("project is running!");
-})
-
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-})
-
-const Discord = require("discord.js");
-const client = new Discord.Client({intents: ["Guilds", "GuildMessages", "MessageContent"], allowedMentions: ["users"]});
-const fs = require("fs");
-const prefix = "?";
-client.commands = new Discord.Collection();
-const commands = fs.readdirSync("./Commands").filter(file => file.endsWith(".js"))
-for(file of commands){
-  const commandName = file.split(".")[0]
-  const command = require(`./Commands/${commandName}`)
-  client.commands.set(commandName, command)
-}
-
-client.on("messageCreate", message => {
-if(message.content.startsWith(prefix)){
-  const args = message.content.slice(prefix.length).trim().split(/ +/g)
-  const commandName = args.shift()
-  const command = client.commands.get(commandName)
-  if(!command) return message.channel.send({content: "That command doesn't exist!"})
-  command.run(client, message, args)
-}
-})
-/*if(message.content === "embed") {
-  let embed = new Discord.EmbedBuilder()
-  .setTitle("This is your embed title")
-  .setDescription("This is your embed description")
-  .setFooter({text: "This is your embed footer"})
-  .setColor("#51C6A5")
-
-  message.channel.send({embeds:[embed]})
-})
-*/
-
-//client.login(process.env.token);
 const express = require("express");
 const app = express();
 const keep_alive = require('./keep_alive.js');
@@ -75,7 +29,7 @@ for (const file of commandFiles) {
 }
 
 client.on("messageCreate", message => {
-  if (message.author.bot) return; // Ignore messages from other bots
+  if (message.author.bot) return; 
 
   if (message.content.startsWith(prefix)) {
     const args = message.content.slice(prefix.length).trim().split(/ +/);
