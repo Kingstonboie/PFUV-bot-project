15e9c76fdde5535a170cf7c6d3b6684100cb0a25
heavensfear <85403286+Kingstonboie@users.noreply.github.com>
2025-01-29 10:11:42 +0000

    sayembed added

---
+++ Commands/sayembed.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "sayembed", 
+  run: (client, message, args) => {
+    const toSay = args.join(" "); 
+    if (!toSay) return message.channel.send({ content: "You have to provide something!" }); 
+
+   
+    const embed = new Discord.EmbedBuilder()
+      .setDescription(toSay) 
+      .setColor("#51C6A5") 
+      .setFooter({ text: `Requested by ${message.author.tag}`, iconURL: message.author.displayAvatarURL() }); 
+
+    message.channel.send({ embeds: [embed] });
+  },
+};
